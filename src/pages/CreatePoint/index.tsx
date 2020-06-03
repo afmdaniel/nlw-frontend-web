e2903788b2c2e463454728126e6bb9062a0c0703
afmdanielwin <danielfarias380@gmail.com>
2020-06-03 19:36:20 -0300

    Push user to home page

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, ChangeEvent, FormEvent} from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import { Map, TileLayer, Marker } from 'react-leaflet'
 import { LeafletMouseEvent } from 'leaflet'
@@ -48,6 +48,8 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState<string>('0')
     const [selectedItems, setSelectedItems] = useState<number[]>([])
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0])
+
+    const history = useHistory()
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
@@ -145,12 +147,7 @@ const CreatePoint = () => {
         await api.post('points', data)
         alert('Ponto de Coleta cadastrado com sucesso!')
 
-        setFormData({ name: '', email: '', whatsapp: ''})
-        setSelectedUF('0')
-        setSelectedCity('0')
-        setSelectedPosition([0, 0])
-        setSelectedItems([])
-
+        history.push('/')
     }
 
     return (
